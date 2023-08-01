3fd9b26b30e5bfbfc3ae8e404159c2a6ab2b96b1
allanzhong <allanzhong@tencent.com>
2023-08-01 19:55:47 +0800

    测试ScrollView的布局参数fillViewport的效果

---
+++ module/m_test/src/main/java/com/aqinn/demo/complexbuild/mtest/TestScrollViewActivity.kt
@@ -0,0 +1,26 @@
+package com.aqinn.demo.complexbuild.mtest
+
+import android.os.Bundle
+import android.view.ViewStub
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class TestScrollViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_scroll_view)
+
+        bindView()
+        initView()
+    }
+
+    private fun bindView() {
+
+    }
+
+    private fun initView() {
+
+    }
+
+}
